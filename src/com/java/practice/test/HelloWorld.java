3fafecd34f6d151d90c32b4bf5c7a0fdc9914675
mukeshgupta-coder <eng.mukeshgupta@gmail.com>
2022-04-22 09:06:07 +0530

    Corrected mistake of deleting Hello World

--- src/com/java/practice/test/HelloWorld.java
+++ src/com/java/practice/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.java.practice.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World in 2022");
+		System.out.println("Hello World in 2022!");
 	}
 
 }
