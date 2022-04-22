ce1b58326ac49660870f8fc72c5e625bfe2785ad
mukeshgupta-coder <eng.mukeshgupta@gmail.com>
2022-04-22 08:54:55 +0530

    Hello world in 2022

--- src/com/java/practice/test/HelloWorld.java
+++ src/com/java/practice/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.java.practice.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World in 2022");
 	}
 
 }
