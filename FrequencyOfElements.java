2190578ccf8c1783783165643c2fd5c7defd5920
mukeshgupta-coder <104180778+mukeshgupta-coder@users.noreply.github.com>
2022-05-17 00:06:04 +0530

    find frequency of each element in the array

---
+++ FrequencyOfElements.java
@@ -0,0 +1,32 @@
+package JavaPracticeExampleArray;
+
+public class FrequencyOfElements {
+
+	public static void main(String[] args) {
+		int[] a = new int[] { 1, 3, 4, 3, 3, 2, 2, 2, 5, 2 };
+
+		int[] f = new int[a.length];
+		int visit = -1;
+		for (int i = 0; i < a.length; i++) {
+			int count = 1;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] == a[j]) {
+					count++;
+					f[j] = visit;
+				}
+			}
+			if (f[i] != visit) {
+				f[i] = count;
+			}
+		}
+		System.out.println("-----------------");
+		System.out.println("  Element | Frequency  ");
+		for (int i = 0; i < f.length; i++) {
+			if (f[i] != visit) {
+				System.out.println("      " + a[i] + "      " + f[i]);
+			}
+			
+		}
+		System.out.println("------------------");
+	}
+}
