d7193fd78b2de022bdf295f08a194ce800cf5c35
mukeshgupta-coder <104180778+mukeshgupta-coder@users.noreply.github.com>
2022-05-25 00:46:50 +0530

    reverse pyramind pattern in java

---
+++ ReversePyramidStarPattern.java
@@ -0,0 +1,19 @@
+package JavaPracticeExample;
+
+import java.util.Scanner;
+
+public class ReversePyramidStarPattern {
+
+	public static void main(String[] args) {
+		System.out.print("Enter the No. of lines ");
+		Scanner objScanner = new Scanner(System.in);
+		int N = objScanner.nextInt();
+		int i,j,k;
+		
+		for(i=0;i<N;i++) {
+			for(j= )
+		}
+
+	}
+
+}
