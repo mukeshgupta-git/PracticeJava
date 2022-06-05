b3714baea9e77844a7f1b9c5e504fd0c224da695
mukeshgupta-coder <104180778+mukeshgupta-coder@users.noreply.github.com>
2022-06-05 23:57:09 +0530

    Trim function in string

---
+++ StringTrim.java
@@ -0,0 +1,11 @@
+package javaString;
+
+public class StringTrim {
+
+	public static void main(String[] args) {
+		String s1 = "  hello string   ";
+		System.out.println(s1 + "welcome");
+		System.out.println(s1.trim() + "welcome");// with trim()
+	}
+
+}
